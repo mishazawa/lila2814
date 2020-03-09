d5dab7e7dd4be0172cc7ab14532c9b0b572413dc
mishazawa <macintoshkrug@gmail.com>
2020-03-09 23:43:23 +0200

    animation for walk

--- src/game/utils/Character.js
+++ src/game/utils/Character.js
@@ -1,5 +1,8 @@
+import * as p5 from 'p5'
+
 import { Renderable } from './Renderable';
 import { TILE_SIZE, PLAYER_STATE } from "../constants";
+
 
 export class Character extends Renderable {
   constructor (renderer, props) {
@@ -7,24 +10,31 @@ export class Character extends Renderable {
     this.spot = 0;
     this.rest = 0;
 
+
     this.state = PLAYER_STATE.idle;
+    this.position = this.assingPosition(props.gameState.field.tiles[this.spot].coords())
+    this.velocity = renderer.createVector(1, 0)
+
 
     this.offset = 0;
     this.currentFrame = 0;
     this.frameCount = 0;
+
+    this.rollNumber = 0;
+    this.nextSpot = this.position;
   }
 
+
   update () {
+    this.animateWalk(this.state);
     this.frameLimiter();
   }
 
   render () {
-    const { field } = this.gameState;
-
-    const { x, y, direction } = field.tiles[this.spot].coords();
+    const { direction } = this.gameState.field.tiles[this.spot].coords()
 
     this.renderer.push();
-    this.renderer.translate(x, y);
+    this.renderer.translate(this.position);
     this.mirrorTile(direction);
     this.renderTile();
     this.renderer.pop();
@@ -32,20 +42,29 @@ export class Character extends Renderable {
     this.update();
   }
 
-  move = async (rollNumber) => {
-    while (rollNumber !== 0) {
-      this.rest -= 1;
-      this.spot += 1;
-      this.state = PLAYER_STATE.walk;
-      await this.playWalkAnimation();
-      rollNumber -= 1;
-    }
-    this.state = PLAYER_STATE.idle;
+  move = (rollNumber) => {
+    if (rollNumber <= 0) return;
+    this.rollNumber = rollNumber;
+
+    this.succSpot();
+    this.setState(PLAYER_STATE.walk);
+
+    this.nextSpot = this.assingPosition(this.gameState.field.tiles[this.spot].coords())
+
+    this.velocity = this.assingVelocity(
+      this.nextSpot,
+      this.position,
+      this.gameState.characters[this.username].config.fps.walk
+    );
   }
 
-  playWalkAnimation = () => new Promise((res) => {
-    setTimeout(res, 1000);
-  })
+  animateWalk = () => {
+    if (this.state !== PLAYER_STATE.walk) return;
+    if (Math.round(this.position.dist(this.nextSpot)) !== 0) return this.position.add(this.velocity);
+    this.resetVelocity();
+    this.setState(PLAYER_STATE.idle);
+    this.move(this.rollNumber - 1);
+  }
 
   mirrorTile = (direction) => {
     if (direction !== -1)  return;
@@ -69,4 +88,18 @@ export class Character extends Renderable {
       this.currentFrame = 0;
     }
   }
+
+  setState = (state) => this.state = state
+
+  succSpot = () => {
+    this.spot += 1;
+    this.rest -= 1;
+  }
+
+  assingPosition = ({x, y, direction}) => this.renderer.createVector(x, y, direction);
+
+  assingVelocity = (a, b, multiplier) => p5.Vector.sub(a, b).normalize().mult(multiplier * 0.2);
+
+  resetVelocity = () => this.velocity = this.renderer.createVector();
+
 }
